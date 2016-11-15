bd9af36aed00d95d215370febdfae94333184c25
SharanyaVR <sharanyavr93@gmail.com>
2016-11-15 14:53:09 -0800

    appended userinfo with training data

--- FeatureGeneration.py
+++ FeatureGeneration.py
@@ -1,8 +1,8 @@
 import pandas as pa
 
-train_data = pa.read_csv("./bytecup2016data/mini_train_data.txt", sep ="\t", names=["qid", "uid", "label"])
-train_question_features = pa.read_csv("./bytecup2016data/mini_ques_info.txt", sep ="\t", names = ["qid", "tag", "wordid", "charid", "upvotes", "totalanswers", "topanswers"])
-train_user_features = pa.read_csv("./bytecup2016data/mini_user_info.txt", sep ="\t", names=["uid", "etags", "wordid", "charid"])
+train_data = pa.read_csv("./bytecup2016data/invited_info_train.txt", sep ="\t", names=["qid", "uid", "label"])
+train_question_features = pa.read_csv("./bytecup2016data/question_info.txt", sep ="\t", names = ["qid", "tag", "wordid", "charid", "upvotes", "totalanswers", "topanswers"])
+train_user_features = pa.read_csv("./bytecup2016data/user_info.txt", sep ="\t", names=["uid", "etags", "wordid", "charid"])
 
 # print train_question_features.shape
 # print train_user_features.shape
@@ -26,7 +26,7 @@ i=0
 for i in range(train_question_features['qid'].size):
     train_question_features.loc[i, train_question_features.loc[i, 'tag']] = 1
     i += 1
-print train_question_features
+# print train_question_features
 '''================================================='''
 
 
@@ -38,5 +38,22 @@ for j in e_tag_arr:
     train_user_features[j]=0
 
 i=0
-while i < train_user_features['uid'].size:
-    train_user_features
+size =  train_user_features['uid'].size
+while(i < size):
+    temp_int_arr = train_user_features.loc[i, 'etags'].split('/')
+    for temp_tag in temp_int_arr:
+        train_user_features.loc[i, temp_tag] = 1
+    i += 1
+
+# print train_user_features
+''' ================================================= '''
+
+
+''' Drop the aggregated 'tag' columns from both user and question data frames '''
+train_user_features = train_user_features.drop('etags', axis=1)
+train_question_features = train_question_features.drop('tag', axis =1)
+# print train_user_features
+# print train_question_features
+
+train_data = pa.merge(train_data, train_user_features, on='uid')
+print train_data
