8ac1e0ad8d1b45dbb299fee2b7fb21f2c462a7ab
Disha Punjabi <dpunjabi@usc.edu>
2016-12-12 19:03:52 -0800

    Create eval_ndcg_on_results.py

---
+++ FinalCode/eval_ndcg_on_results.py
@@ -0,0 +1,27 @@
+'''
+Evaluating ndcg score on the predicted results
+Note: (i) ndcg.py is assumed to be in ./bytecup2016data and
+(ii) change path to point to the csv file that has to be evaluated
+'''
+import sys
+sys.path.insert(0, './bytecup2016data/')
+import ndcg as ndcg
+path = "f3.csv"
+finalcsv = pa.read_csv(path, sep=",",names=["qid","uid","label"] ) 
+mydict = finalcsv.sort_values('label', ascending=False).groupby('qid')['uid'].apply(list).to_dict()
+ndcg_5 = 0
+ndcg_10 = 0
+count = 0 
+for key in mydict:
+    user_list= mydict[key]
+    ranking = []
+    for user in user_list:
+        value = training_data_formatted.loc[(training_data_formatted['qid'] == key) & (training_data_formatted['uid'] == user)]
+        ranking.append(value['label'].values[0])
+    ndcg_5 += ndcg.ndcg_at_k(ranking, 5)
+    ndcg_10 += ndcg.ndcg_at_k(ranking, 10)
+    count+=1
+ndcg_5/=count
+ndcg_10/=count
+final_score = (ndcg_5+ndcg_10)*0.5
+print final_score
